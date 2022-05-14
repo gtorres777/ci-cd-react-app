fd7c97c8162a35e7af64c62ff3d1d75faf85034b
gtorres777 <gustavo.torres.d@tecsup.edu.pe>
2022-05-14 05:12:17 -0500

    testing2 github actions

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn CI/CD
+          Learn devops
         </a>
       </header>
     </div>
