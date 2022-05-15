4b247a7ffe46a830e25dc1e005f3acd9490cc09a
gtorres777 <gustavo.torres.d@tecsup.edu.pe>
2022-05-14 23:29:26 -0500

    testing3 github actions

---
+++ cypress/integration/app.spec.js
@@ -0,0 +1,10 @@
+/* global cy */
+
+describe('E2E', function() {
+    it('renders learn devops link', () => {
+      cy.visit('http://localhost:3000/ci-cd-react-app')
+      cy.contains(/learn devops/i)
+    })
+
+})
+
