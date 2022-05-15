4497fc3634cbb18f4879f4e322e7559216b91db5
gtorres777 <gustavo.torres.d@tecsup.edu.pe>
2022-05-15 02:37:01 -0500

    parallelize2 github actions

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -1,10 +1,8 @@
 /* global cy */
 
-describe('E2E', function() {
-    it('renders learn devops link', () => {
-      cy.visit('http://localhost:3000/ci-cd-react-app')
-      cy.contains(/learn aws/i)
-    })
-
+describe('E2E', function () {
+  it('renders learn devops link', () => {
+    cy.visit('http://localhost:3000/ci-cd-react-app')
+    cy.contains(/learn aws/i)
+  })
 })
-
